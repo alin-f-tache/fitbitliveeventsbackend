6532c6ea99e4ab90d48feabaad784c98a39da97f
Ana-Maria CĂLINESCU (87527) <ana_maria.calinescu@stud.acs.upb.ro>
2020-05-27 12:19:54 +0300

    final

--- fle_backend/src/main/java/live_events/fle_backend/UserRoleController.java
+++ fle_backend/src/main/java/live_events/fle_backend/UserRoleController.java
@@ -17,18 +17,21 @@ public class UserRoleController {
     }
 
     @RequestMapping(value = "/addUserRole", method = RequestMethod.POST)
-    public void addUserRole(@RequestParam(value = "eventid", defaultValue = "0000") String eventId,
+    public String addUserRole(@RequestParam(value = "eventid", defaultValue = "0000") String eventId,
                             @RequestParam(value = "username", defaultValue = "user_1") String username,
                             @RequestParam(value = "role", defaultValue = "organizer") String role) {
         this.jdbcTemplate.update("INSERT into UsersRole values(?, ?, ?)", eventId, username, role);
 
+        return "Succes";
     }
 
     @RequestMapping(value = "/removeUserRole", method = RequestMethod.DELETE)
-    public void removeUserRole(@RequestParam(value = "eventid", defaultValue = "0000") String eventId,
+    public String removeUserRole(@RequestParam(value = "eventid", defaultValue = "0000") String eventId,
                                @RequestParam(value = "username", defaultValue = "user_1") String username,
                                @RequestParam(value = "role", defaultValue = "organizer") String role) {
         this.jdbcTemplate.update("DELETE FROM UsersRole WHERE EventId = ? and Username = ? and Role = ?", eventId, username, role);
+
+        return "Succes";
     }
 
     @RequestMapping(value = "/listAllEvents", method = RequestMethod.GET)
@@ -54,5 +57,13 @@ public class UserRoleController {
         return this.jdbcTemplate.queryForList("SELECT * FROM UsersRole u" +
                 " JOIN Events e ON u.EventId =  e .Id" +
                 " WHERE u.Username = ? AND u.Role = ? AND e.Date > CURDATE();", username, role);
+    }
+
+    @RequestMapping(value = "/fetchEventUser", method = RequestMethod.GET)
+    public List<Map<String, Object>> fetchEventUser(@RequestParam(value = "id", defaultValue = "0000") String id,
+                                                           @RequestParam(value = "role", defaultValue = "all") String role) {
+        return this.jdbcTemplate.queryForList("SELECT * FROM UsersRole u" +
+                " JOIN Events e ON u.EventId =  e .Id" +
+                " WHERE u.EventId = ? AND u.Role = ? AND e.Date > CURDATE();", id, role);
     }
 }
